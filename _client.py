3ccdd2ddb962854c1cdd68aa0525dd2072a087f0
Mihail Georgiev <mihaildono@gmail.com>
2024-04-01 16:05:59 +0300

    Add example client

---
+++ _client.py
@@ -0,0 +1,8 @@
+from binance.client import Client
+
+# Remove _ from the beginning of the file name and replace key and secret with your own
+
+api_key = "___YOUR_API_KEY___"
+api_secret = "___YOUR_API_KEY___"
+
+binance_client = Client(api_key, api_secret)
